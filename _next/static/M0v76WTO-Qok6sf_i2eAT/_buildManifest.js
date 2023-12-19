50aa2368c27fc96d40c5c7c39a0477df21805ea4
Christophe Lopez <christophe.lopez@acseo-conseil.fr>
2023-12-19 17:06:53 +0100

    first commit

---
+++ _next/static/M0v76WTO-Qok6sf_i2eAT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:e,source:"/",destination:"/home"},{has:e,source:"/admin",destination:"/admin/index.html"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-deeb844d2074b9d8.js"],sortedPages:["/_app","/_error"]}}(void 0),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
